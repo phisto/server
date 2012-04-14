795b889c1fc24a898f95e99b9a455cfe0dbe1d24
Matthieu Guffroy <mattgu74@gmail.com>
2012-04-14 15:05:13 +0200

    paybox retour

--- paybox_retour.php
+++ paybox_retour.php
@@ -47,8 +47,8 @@ function PbxVerSign( $qrystr, $keyfile ) {                  // verification sign
 function mymail() {
 	global $_GET, $CheckSig;
      $to      = 'mattgu74@gmail.com';
-     $subject = 'CommandSports.fr : Paiement en ligne : Action : '. $_GET['action'];
-     $message = 'La signature a renvoyé : '. $CheckSig." \r\n" . $message." \r\n" . var_export($_GET,true);
+     $subject = 'CommandSports.fr : Paiement en ligne';
+     $message = 'La signature a renvoyé : '. $CheckSig." \r\n" . var_export($_GET,true);
      $headers = 'From: payutc@assos.utc.fr' . "\r\n" .
      'Reply-To: mattgu74@gmail.com' . "\r\n" .
      'X-Mailer: PHP/' . phpversion();
@@ -60,10 +60,6 @@ function mymail() {
 $pos = strrpos( $_SERVER["REQUEST_URI"], '?' );                         // cherche dernier separateur
 $data = substr( $_SERVER["REQUEST_URI"], $pos+1 ); 
 
-if($_GET['action'] == 'retour') {                 
-	$data = substr( $data, 14 ); 
-}
-
 // Verification de la signature (1 = BON)
 $CheckSig = PbxVerSign( $data, '../pubkey.pem' );
 
@@ -74,135 +70,6 @@ $cmd=$_GET['ident'];
 
 $auto=$_GET['auto'];
 $trans=$_GET['trans'];
-
-
-if($_GET['action'] == 'erreur') { // On a une erreur on se fout un peu de la signature
-	$num_err=$_GET['NUMERR'];
-	$resa->paiementErreur($num_err);
-	if ( $num_err == -1 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : erreur de lecture des paramètres via stin. <br>");
-		}
-	else if ( $num_err == -2 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : erreur d'allocation mémoire. <br>");
-		}
-	else if ( $num_err == -3 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : erreur de lecture des paramètres QUERY_STRING ou CONTENT_LENGTH. <br>");
-		}
-	else if ( $num_err == -4 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_RETOUR, PBX_ANNULE, PBX_REFUSE ou PBX_EFFECTUE sont trop longs (<150 caractères). <br>");
-		}
-	else if ( $num_err == -5 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : ouverture de fichiers (si PBX_MODE contient 3) : fichier local inexistant, non trouvé ou erreur d'accès. <br>");
-		}
-
-	else if ( $num_err == -6 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : ouverture de fichiers (si PBX_MODE contient 3) : fichier local mal formé, vide ou ligne mal formatée. <br>");
-		}
-	else if ( $num_err == -7 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : Il manque une variable obligatoire. <br>");
-		}
-	else if ( $num_err == -8 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : Une variable numérique contient un caractère non numérique. <br>");
-		}
-	else if ( $num_err == -9 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_SITE contient un numéro de site qui ne fait pas exactement 7 caractères. <br>");
-		}
-	else if ( $num_err == -10 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_RANG contient un numéro de rang qui ne fait pas exactement 2 caractères. <br>");
-		}
-	else if ( $num_err == -11 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_TOTAL fait plus de 10 ou moins de 3 caractères numériques. <br>");
-		}
-	else if ( $num_err == -12 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_LANGUE ou PBX_DEVISE contient un code qui ne fait pas exactement 3 caractères. <br>");
-		}
-	else if ( $num_err == -16 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_PORTEUR ne contient pas une adresse e-mail valide. <br>");
-		}
-	else if ( $num_err == -17 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_CLE ne contient pas une clé (mot de passe) valide. <br>");
-		}
-	else if ( $num_err == -18 )
-		{
-		print ("<center><b><h2>Erreur appel PAYBOX.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : PBX_RETOUR : Données à retourner inconnues. <br>");
-		}
-
-	print("<b>Numéro de l'erreur : </b>$num_err\n");
-	exit();
-} else if($_GET['action'] == 'annule') { // On a une annulation
-		//mymail();
-		if($CheckSig)
-			$resa->paiementAnnule();
-		else
-			echo 'Erreur Signature';
-		
-} else if($_GET['action'] == 'refuse') { // la transaction a ete refuse
-		//mymail();
-		if($CheckSig)
-			$resa->paiementRefuse();
-		else
-			echo 'Erreur Signature';
-} else if($_GET['action'] == 'effectue') { // a priori ça a l'air bon
-		//mymail();
-		if($CheckSig)
-			$resa->paiementEffectue($trans,$token,$auto);
-		else
-			echo 'Erreur Signature';
-} else if($_GET['action'] == 'retour') { // le bordel envoit direct du serveur :)
-		if($CheckSig)
-			$resa->paiementEffectue($trans,$token,$auto);
-		else
-			echo 'Erreur Signature';
-		exit(); // SUR CETTE URL IL NE FAUT RIEN RETOURNER
-} else { // Je sais pas 
-	echo 'Erreur inconnu';
-}
-
-
 
 
 $db = Db_buckutt::getInstance();
